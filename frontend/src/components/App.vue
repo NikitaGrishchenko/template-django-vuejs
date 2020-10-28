82ecf3717a587a2d63377d34309451d2d10bc7cb
NikitaGrishchenko <grishchenkonikita@mail.ru>
2020-10-28 23:37:29 +0300

    refactoring

---
+++ frontend/src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="vue-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style lang="sass">
+@import '../../static/sass/app'
+body
+  color: red
+</style>
