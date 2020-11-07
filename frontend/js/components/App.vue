20ac9dae6f2ed08c1b9d75c2bc4b4d9e5abce2e1
NikitaGrishchenko <grishchenkonikita@mail.ru>
2020-11-07 20:19:10 +0300

    test API

--- frontend/js/components/App.vue
+++ frontend/js/components/App.vue
@@ -5,8 +5,24 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      todos: null
+    };
+  },
+
+  created() {
+    axios
+      .get("www/")
+      .then(response => {
+        this.todos = response.data;
+        console.log(this.todos);
+      })
+      .catch(error => console.log(error));
+  }
 };
 </script>
 
